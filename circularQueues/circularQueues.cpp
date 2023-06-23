fdd13aa9c4b08f110d2935d520195d11769289de
Muhammad Zuhair Zaidan <muhammad.zuhair.ft22@mail.umy.ac.id>
2023-06-23 16:29:09 +0700

    untuk mengecek antrian penuh atau tidak

--- circularQueues/circularQueues.cpp
+++ circularQueues/circularQueues.cpp
@@ -41,9 +41,103 @@ public:
 		//cek apakah antrian kosong
 		if (FRONT == -1) {
 			cout << "Queue underflow\n";
-			return
+			return;
+		}
+		cout << "\nThe element deleted from the queue is: " << queue_array[FRONT] << "\n";
+
+		//cek antrian hanya memiliki satu elemen
+		if (FRONT == REAR) {
+			FRONT = -1;
+			REAR = -1;
+		}
+		else {
+			//jika elemen yang dihapus berada di posisi terakhir array kembali ke awal array
+			if (FRONT == max - 1)
+				FRONT = 0;
+			else
+				FRONT = FRONT + 1;
 		}
 	}
+	void display() {
+		int FRONT_position = FRONT;
+		int REAR_position = REAR;
+		// cek apakah antrian kosong
+		if (FRONT == -1) {
+			cout << "Queue is empty\n";
+			return;
+		}
+		cout << "\nelemen in the queue are...\n";
+
+		//jika FRONT_position <= REAR_position iterasi dari FRONT hingga REAR
+		if (FRONT_position <= REAR_position) {
+			while (FRONT_position <= REAR_position) {
+				cout << queue_array[FRONT_position] << " ";
+				FRONT_position++;
+			}
+			cout << endl;
+		}
+		else {
+			// Jika FRONT_position > REAR_position, iterasi dari FRONT hingga akhir array
+			while (FRONT_position <= max - 1) {
+				cout << queue_array[FRONT_position] << "	";
+				FRONT_position++;
+			}
+			FRONT_position = 0;
+
+			//iterasi dari awal array hingga REAR
+			while (FRONT_position <= REAR_position) {
+				cout << queue_array[FRONT_position] << "	";
+				FRONT_position++;
+			}
+			cout << endl;
+		}
+	
 
 };
+
+	int main() {
+		Queues q;
+		char ch;
+
+		while (true) {
+			try {
+				cout << "Menu" << endl;
+				cout << "1. Implement Insert Operation" << endl;
+				cout << "2. Implement Delete Operation" << endl;
+				cout << "3. Display Values" << endl;
+				cout << "4. Exit" << endl;
+				cout << "Enter your choice (1-4): ";
+				cin >> ch;
+				cout << endl;
+
+				switch (ch) {
+				case '1': {
+					q.insert();
+					break;
+				}
+				case '2': {
+					q.remove();
+					break;
+				}
+				case '3': {
+					q.display();
+					break;
+				}
+				case '4': {
+					return 0;
+				}
+				default: {
+					cout << "Invalid Option!" << endl;
+					break;
+				}
+				}
+			}
+			catch (exception& e) {
+				cout << "Check for the values entered." << endl;
+			}
+		}
+
+		return 0;
+	};
+
 
