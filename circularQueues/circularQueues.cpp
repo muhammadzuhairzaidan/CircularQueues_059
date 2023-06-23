e9f10a044c01ceeb6efef8bf60b26e43668de4c1
Muhammad Zuhair Zaidan <muhammad.zuhair.ft22@mail.umy.ac.id>
2023-06-23 15:04:06 +0700

    menampilkan Enter a number

--- circularQueues/circularQueues.cpp
+++ circularQueues/circularQueues.cpp
@@ -18,6 +18,24 @@ public:
 		cout << endl;
 
 		//cek apakah antrian penuh
+		if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
+			cout << "\nQueue overflow\n";
+			return;
+		}
+		//cek apakah antrian kosong
+		if (FRONT == -1) {
+			FRONT = 0;
+			REAR = 0;
+		}
+		else {
+			// jika REAR berada di posisi terakhir array, kembali ke awal array
+			if (REAR == max - 1)
+				REAR = 0;
+			else
+				REAR = REAR + 1;
+
+		}
+		queue_array[REAR] = num;
 	}
 
 };
